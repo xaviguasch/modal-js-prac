6a895ddec567dd12192445501631c61a52a73a14
xaviguasch <xaviguasch@gmail.com>
2021-02-22 20:48:23 +0100

    added functionality to the modal

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-'use strict';
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+btnsOpenModal.forEach((btnOpenModal) => btnOpenModal.addEventListener('click', openModal))
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
